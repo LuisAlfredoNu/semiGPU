174f62f5952f076528caee95e41e51af99a1cd1f
LuisAlfredoNu <luis.alfredo.nu@gmail.com>
2021-06-22 17:01:38 -0500

    init MNDO files of parameters

---
+++ src/common/MNDO_parameters.cpp
@@ -0,0 +1,22 @@
+/* 
+   List of parameters for semiempiric method MNDO
+*/
+#ifndef _MNDO_PARAMETERS_CPP_
+#define _MNDO_PARAMETERS_CPP_
+
+#include "MNDO_parameters.h"
+/***************************************************************************************/ 
+/***************************************************************************************/ 
+
+MNDOparameter::MNDOparameter(){
+
+   zs[1]=1.3319670;
+//    Data for element 6 - Carbon
+
+   zs[6]=1.7875370;
+   zp[6]=1.7875370;
+}
+
+
+
+#endif // _MNDO_PARAMETERS_CPP_
---
+++ src/common/MNDO_parameters.h
@@ -0,0 +1,16 @@
+/* 
+   List of parameters for semiempiric method MNDO
+*/
+#ifndef _MNDO_PARAMETERS_H_
+#define _MNDO_PARAMETERS_H_
+
+/***************************************************************************************/ 
+class MNDOparameter{
+/***************************************************************************************/ 
+public:
+   MNDOparameter();
+   double zs[109];
+   double zp[109];
+};
+
+#endif // _MNDO_PARAMETERS_H_
