b7d50ebaa619d4a35919c3b721683ba17459824f
LuisAlfredoNu <luis.alfredo.nu@gmail.com>
2021-08-10 15:30:58 -0500

    update test for new methods

--- src/test/test_twoCenterIntegral.cc
+++ src/test/test_twoCenterIntegral.cc
@@ -3,11 +3,15 @@
 using std::cout;
 using std::endl;
 using std::cerr;
+#include <iomanip>
+using std::setw;
+using std::setprecision;
 #include <string>
 using std::string;
 #include <vector>
 using std::vector;
 
+#include "atom.h"
 #include "atomicOrbitals.h"
 #include "MNDO_parameters.h"
 
@@ -25,31 +29,36 @@ int main (int argc, char *argv[])
   AtomicOrbital orbitalA,orbitalB,orbitalC,orbitalD;
 
   double coorA[3] = {0.0,0.0,0.0};
-  int angularMomA[3] = {0,0,0};
+  int angularMomA[3] = {1,0,0};
   orbitalA.SetElement( 6);
   orbitalA.SetCoordinates(coorA);
   orbitalA.SetAngularMomentum(angularMomA);
-  orbitalA.SetNAtom(0);
+  orbitalA.SetIndexAtom(0);
 
-  int angularMomB[3] = {0,0,0};
+  int angularMomB[3] = {1,0,0};
   orbitalB.SetElement( 6);
   orbitalB.SetCoordinates(coorA);
   orbitalB.SetAngularMomentum(angularMomB);
-  orbitalB.SetNAtom(0);
+  orbitalB.SetIndexAtom(0);
 
   double coorC[3] = {1.0,1.0,1.5};
   int angularMomC[3] = {0,0,0};
   orbitalC.SetElement( 6);
   orbitalC.SetCoordinates(coorC);
   orbitalC.SetAngularMomentum(angularMomC);
-  orbitalC.SetNAtom(1);
+  orbitalC.SetIndexAtom(1);
 
-  int angularMomD[3] = {0,0,0};
+  int angularMomD[3] = {1,0,0};
   orbitalD.SetElement( 6);
   orbitalD.SetCoordinates(coorC);
   orbitalD.SetAngularMomentum(angularMomD);
-  orbitalD.SetNAtom(1);
+  orbitalD.SetIndexAtom(1);
 
+  cout << "orbitalA.angularMomentumInt = " << orbitalA.angularMomentumInt << endl;
+  cout << "orbitalB.angularMomentumInt = " << orbitalB.angularMomentumInt << endl;
+  cout << "orbitalC.angularMomentumInt = " << orbitalC.angularMomentumInt << endl;
+  cout << "orbitalD.angularMomentumInt = " << orbitalD.angularMomentumInt << endl;
+  cout << endl;
   // Init MNDO parameters
   MNDOparameter MNDOpara;
 
@@ -59,6 +68,37 @@ int main (int argc, char *argv[])
   double result =  twoCIntegral.ComputeTwoCenterIntegral(orbitalA,orbitalB,orbitalC,orbitalD);
   cout << "result = " << result << endl;
 
+  vector<Atom> molecule (2,Atom());
+
+  molecule[0].setCoordinates(coorA[0],coorA[1],coorA[2]);
+  molecule[0].setAtomNumber(6);
+  molecule[1].setCoordinates(coorC[0],coorC[1],coorC[2]);
+  molecule[1].setAtomNumber(6);
+
+  ListAtomicOrbitals AOs;
+  AOs.SetOrbitals(molecule);
+
+  double**** all2CenterIntegral = NULL;
+  if (TwoCenterIntegral::Alloc4AllTwoCenterIntegral(molecule,all2CenterIntegral)){
+    cout << "Correct Alloc" << endl;
+  }else{
+    cout << "Bad Alloc" << endl;
+  }
+
+  twoCIntegral.ComputeAllTwoCenterIntegral(AOs.orbital,all2CenterIntegral);
+
+  cout << std::fixed << setprecision(4);
+  for (int i=0;i<2;i++) {
+    for (int j=0;j<=i;j++) {
+      for (int k=0;k<10;k++) {
+        for (int l=0;l<10;l++) {
+          cout << setw(12) << all2CenterIntegral[i][j][k][l];
+        }
+        cout << endl;
+      }
+        cout << endl;
+    }
+  }
 
 	return EXIT_SUCCESS;
 }
